40cdb71ee4513a032e21b212f196866c34eb35e1
Lars Westermann <git@lars-westermann.de>
2022-02-10 14:08:32 +0000

    Add working attestation

--- create_image.sh
+++ create_image.sh
@@ -3,17 +3,22 @@
 set -x -a -e
 trap "echo Unexpected error! See log above; exit 1" ERR
 
+shopt -s expand_aliases
+export ALIAS="$HOME/.bash_aliases"
+source "$ALIAS"
+type -a scone || error "alias 'scone' undefined. Please add this to your .bashrc first."
+
+
 # CONFIG Parameters (might change)
 
-export IMAGE=${IMAGE:-lld_server_image}
-export SCONE_CAS_ADDR="4-2-1.scone-cas.cf"
+export IMAGE="pixix4/lld-scone-sqlite"
+export SCONE_CAS_ADDR="5-7-0.scone-cas.cf"
 export DEVICE="/dev/sgx"
 
-export CAS_MRENCLAVE="4cd0fe54d3d8d787553b7dac7347012682c402220acd062e4d0da3bbe10a1c2c"
+export CAS_MRENCLAVE="3061b9feb7fa67f3815336a085f629a13f04b0a1667c93b14ff35581dc8271e4"
 
-export CLI_IMAGE="registry.scontain.com:5050/sconecuratedimages/kubernetes:hello-k8s-scone0.1"
-export LLD_IMAGE="pixix4/lld-scone-sqlite"
-export LLD_MRENCLAVE="67b8017f7083435cb614b87c8daa14303f741a10a2a0bbf5dfabec777cf629b9"
+export CLI_IMAGE="registry.scontain.com:5050/community/cli"
+export LLD_MRENCLAVE="3893af22c62ed83a811ecb5a471e0c96f5f558cd8bdfde876493a1a65bde710b"
 
 # create random and hence, uniquee session number
 LLD_SESSION="LldSession-$RANDOM-$RANDOM-$RANDOM"
@@ -35,9 +40,8 @@ fi
 
 # attest cas before uploading the session file, accept CAS running in debug
 # mode (-d) and outdated TCB (-G)
-docker run --device=$DEVICE -it $CLI_IMAGE sh -c "
-scone cas attest -G --only_for_testing-debug  $SCONE_CAS_ADDR $CAS_MRENCLAVE >/dev/null \
-&&  scone cas show-certificate" > cas-ca.pem
+scone cas attest -G --only_for_testing-debug --only_for_testing-ignore-signer $SCONE_CAS_ADDR $CAS_MRENCLAVE
+scone cas show-certificate > cas-ca.pem
 
 # ensure that we have self-signed client certificate
 
@@ -49,7 +53,7 @@ fi
 
 MRENCLAVE=$LLD_MRENCLAVE envsubst '$MRENCLAVE $LLD_SESSION' < lld-template.yml > lld_session.yml
 # note: this is insecure - use scone session create instead
-curl -v -k -s --cert client.pem  --key client-key.pem  --data-binary @redis_session.yml -X POST https://$SCONE_CAS_ADDR:8081/session
+curl -v -k -s --cert client.pem  --key client-key.pem  --data-binary @lld_session.yml -X POST https://$SCONE_CAS_ADDR:8081/session
 
 # create file with environment variables
 
